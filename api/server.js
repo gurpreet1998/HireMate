a2e436186b7bbb06de4ea25ec2b13538ffbac8d8
Gurpreet <37729999+gurpreet1998@users.noreply.github.com>
2023-05-13 23:31:54 -0700

    Implemented Controllers

--- api/server.js
+++ api/server.js
@@ -8,8 +8,8 @@ import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+//import cookieParser from "cookie-parser";
+//import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -24,9 +24,9 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+//app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-app.use(cookieParser());
+//app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
