66b7923bdbb9182eada518a928ce51cd3d9adbcd
Gurpreet <37729999+gurpreet1998@users.noreply.github.com>
2023-05-14 01:08:59 -0700

    Added JWT based authentication

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
-//import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 //import cors from "cors";
 
 const app = express();
@@ -26,7 +26,7 @@ const connect = async () => {
 
 //app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-//app.use(cookieParser());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
