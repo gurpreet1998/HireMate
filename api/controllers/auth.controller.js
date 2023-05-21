d7eeee555fc588c5a15108bd4847e7984213410e
Gurpreet <37729999+gurpreet1998@users.noreply.github.com>
2023-05-21 15:19:34 -0700

    Code Refactor

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,9 +30,9 @@ export const login = async (req, res, next) => {
 
     if (!user) return next(createError(404, "User not found!"));
 
-    // const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-    // if (!isCorrect)
-    //   return next(createError(400, "Wrong password or username!"));
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
       {
